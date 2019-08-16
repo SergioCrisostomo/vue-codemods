2d2c6e796214030b8c2d0c588cddc7203bcf1ff0
Sergio Crisostomo <sergiosbox@gmail.com>
2019-08-16 19:01:32 +0200

    Add boilerplate config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {}
+}
