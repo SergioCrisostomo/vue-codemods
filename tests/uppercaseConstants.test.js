5c835eeee6c7e07feaf9b5bddda77f4036a0e43c
Sergio Crisostomo <sergiosbox@gmail.com>
2019-08-20 20:24:26 +0200

    Add test for different option combinations

--- tests/uppercaseConstants.test.js
+++ tests/uppercaseConstants.test.js
@@ -5,20 +5,83 @@ const exec = require('./utils/exec');
 const codemodFolder = 'uppercase_constants';
 
 describe('Uppercase constants', () => {
-  test('Should convert variable names to UPPERCASE_SNAKE_CASE', (done) => {
-    const FILE_PATH = path.join(__dirname, '/test_assets/' + codemodFolder + '/');
-    const TRANSFORMER_PATH = path.join(__dirname, '../transformers/' + codemodFolder + '/');
+  describe('Options: which', () => {
+    test('Should convert recurring variable names to UPPERCASE_SNAKE_CASE', (done) => {
+      const FILE_PATH = path.join(__dirname, '/test_assets/' + codemodFolder + '/');
+      const TRANSFORMER_PATH = path.join(__dirname, '../transformers/' + codemodFolder + '/');
 
-    const beforeTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
-    const transformedFile = fs.readFileSync(FILE_PATH + 'transformed.js', 'utf8');
+      const beforeTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+      const transformedFile = fs.readFileSync(FILE_PATH + 'transformed_multiple.js', 'utf8');
 
-    exec(`node ${TRANSFORMER_PATH}uppercaseConstants.js --path ${FILE_PATH}original.js --options '{"which": "all", "dry": true}'`).then(() => {
-      const afterTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+      exec(
+        `node ${TRANSFORMER_PATH}uppercaseConstants.js --path ${FILE_PATH}original.js --options '{"which": "multiple", "dry": false}'`,
+      ).then(() => {
+        const afterTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
 
-      // restore file
-      fs.writeFileSync(FILE_PATH + 'original.js', beforeTransformation, 'utf8');
-      expect(afterTransformation).toEqual(transformedFile);
-      done();
+        // restore file
+        fs.writeFileSync(FILE_PATH + 'original.js', beforeTransformation, 'utf8');
+        expect(afterTransformation).toEqual(transformedFile);
+        done();
+      });
+    });
+
+    test('Should convert all variable names to UPPERCASE_SNAKE_CASE', (done) => {
+      const FILE_PATH = path.join(__dirname, '/test_assets/' + codemodFolder + '/');
+      const TRANSFORMER_PATH = path.join(__dirname, '../transformers/' + codemodFolder + '/');
+
+      const beforeTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+      const transformedFile = fs.readFileSync(FILE_PATH + 'transformed_all.js', 'utf8');
+
+      exec(
+        `node ${TRANSFORMER_PATH}uppercaseConstants.js --path ${FILE_PATH}original.js --options '{"which": "all", "dry": false}'`,
+      ).then(() => {
+        const afterTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+
+        // restore file
+        fs.writeFileSync(FILE_PATH + 'original.js', beforeTransformation, 'utf8');
+        expect(afterTransformation).toEqual(transformedFile);
+        done();
+      });
+    });
+  });
+
+  describe('Options: dry', () => {
+    test('Should dry and convert recurring variable names to UPPERCASE_SNAKE_CASE', (done) => {
+      const FILE_PATH = path.join(__dirname, '/test_assets/' + codemodFolder + '/');
+      const TRANSFORMER_PATH = path.join(__dirname, '../transformers/' + codemodFolder + '/');
+
+      const beforeTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+      const transformedFile = fs.readFileSync(FILE_PATH + 'transformed_dry.js', 'utf8');
+
+      exec(
+        `node ${TRANSFORMER_PATH}uppercaseConstants.js --path ${FILE_PATH}original.js --options '{"which": "multiple", "dry": true}'`,
+      ).then(() => {
+        const afterTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+
+        // restore file
+        fs.writeFileSync(FILE_PATH + 'original.js', beforeTransformation, 'utf8');
+        expect(afterTransformation).toEqual(transformedFile);
+        done();
+      });
+    });
+
+    test('Should dry and convert all variable names to UPPERCASE_SNAKE_CASE', (done) => {
+      const FILE_PATH = path.join(__dirname, '/test_assets/' + codemodFolder + '/');
+      const TRANSFORMER_PATH = path.join(__dirname, '../transformers/' + codemodFolder + '/');
+
+      const beforeTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+      const transformedFile = fs.readFileSync(FILE_PATH + 'transformed_dry_all.js', 'utf8');
+
+      exec(
+        `node ${TRANSFORMER_PATH}uppercaseConstants.js --path ${FILE_PATH}original.js --options '{"which": "all", "dry": true}'`,
+      ).then(() => {
+        const afterTransformation = fs.readFileSync(FILE_PATH + 'original.js', 'utf8');
+
+        // restore file
+        fs.writeFileSync(FILE_PATH + 'original.js', beforeTransformation, 'utf8');
+        expect(afterTransformation).toEqual(transformedFile);
+        done();
+      });
     });
   });
 });
